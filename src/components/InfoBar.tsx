04160da22d92dbacb9219d648f1480723e6bfe7f
nakada toyohisa <nakada@nuis.ac.jp>
2026-03-30 20:39:57 +0900

    リセットボタンの位置変更と、それに伴い、ControlPanel.tsxの不使用

--- src/components/InfoBar.tsx
+++ src/components/InfoBar.tsx
@@ -11,19 +11,25 @@ export default function InfoBar({ game, scaleBase }: InfoBarProps) {
             className="relative flex items-center border-b-[4px] border-[#948073] mt-2"
             style={{ width: `${52 * scaleBase}px`, height: `${5 * scaleBase}px` }}
         >
-            <div className="w-1/4 text-center flex flex-col justify-center">
+            <div className="w-1/6 text-center flex flex-col justify-center">
+                <button
+                    className="text-[#F7F2EE] bg-gray-500 hover:bg-gray-600 active:bg-gray-700 cursor-pointer"
+                    onClick={game.resetGame}
+                >リセット</button>
+            </div>
+            <div className="w-1/5 text-center flex flex-col justify-center">
                 <span className="text-[#534B44] font-bold text-sm tracking-widest">turn</span>
                 <span className="text-[#534B44] font-bold text-2xl">{game.state.turnNum}</span>
             </div>
-            <div className="w-1/4 text-center flex flex-col justify-center">
-                <span className="text-[#534B44] font-bold text-sm tracking-widest">white 板 残数</span>
+            <div className="w-1/5 text-center flex flex-col justify-center">
+                <span className="text-[#534B44] font-bold text-sm tracking-widest">white板残数</span>
                 <span className="text-[#534B44] font-bold text-2xl">{10 - game.state.n_p0_wall}</span>
             </div>
-            <div className="w-1/4 text-center flex flex-col justify-center">
-                <span className="text-[#534B44] font-bold text-sm tracking-widest">black 板 残数</span>
+            <div className="w-1/5 text-center flex flex-col justify-center">
+                <span className="text-[#534B44] font-bold text-sm tracking-widest">black板残数</span>
                 <span className="text-[#534B44] font-bold text-2xl">{10 - game.state.n_p1_wall}</span>
             </div>
-            <div className="w-1/4 text-center flex flex-col justify-center items-center gap-1">
+            <div className="w-1/5 text-center flex flex-col justify-center items-center gap-1">
                 <span className="text-[#534B44] font-bold text-xs tracking-widest leading-none">AI</span>
                 <select
                     className="text-xs font-bold border rounded px-1 text-gray-700 bg-gray-50 outline-none cursor-pointer text-center"
--- src/components/QuoridorGame.tsx
+++ src/components/QuoridorGame.tsx
@@ -26,41 +26,11 @@ export default function QuoridorGame() {
 
         {/* InfoBar */}
         <InfoBar game={game} scaleBase={scaleBase} />
-        {/* <div
-          className="relative flex items-center border-b-[4px] border-[#948073] mt-2"
-          style={{ width: `${52 * scaleBase}px`, height: `${5 * scaleBase}px` }}
-        >
-          <div className="w-1/4 text-center flex flex-col justify-center">
-            <span className="text-[#534B44] font-bold text-sm tracking-widest">TURN</span>
-            <span className="text-[#534B44] font-bold text-2xl">{game.state.turnNum}</span>
-          </div>
-          <div className="w-1/4 text-center flex flex-col justify-center">
-            <span className="text-[#534B44] font-bold text-sm tracking-widest">WHITE</span>
-            <span className="text-[#534B44] font-bold text-2xl">{10 - game.state.n_p0_wall}</span>
-          </div>
-          <div className="w-1/4 text-center flex flex-col justify-center">
-            <span className="text-[#534B44] font-bold text-sm tracking-widest">BLACK</span>
-            <span className="text-[#534B44] font-bold text-2xl">{10 - game.state.n_p1_wall}</span>
-          </div>
-          <div className="w-1/4 text-center flex flex-col justify-center items-center gap-1">
-            <span className="text-[#534B44] font-bold text-xs tracking-widest leading-none">AI VS</span>
-            <select
-              className="text-xs font-bold border rounded px-1 text-gray-700 bg-gray-50 outline-none cursor-pointer text-center"
-              value={game.state.aiPlayer}
-              onChange={(e) => game.setAiPlayer(e.target.value as any)}
-            >
-              <option value="none">None</option>
-              <option value="white">White</option>
-              <option value="black">Black</option>
-              <option value="both">Both</option>
-            </select>
-          </div>
-        </div>*/ }
       </div>
 
-      <div className="flex flex-col" style={{ width: `${12 * scaleBase}px` }}>
+      {/* <div className="flex flex-col" style={{ width: `${12 * scaleBase}px` }}>
         <ControlPanel game={game} scaleBase={scaleBase} />
-      </div>
+      </div> */}
     </div>
   );
 }
